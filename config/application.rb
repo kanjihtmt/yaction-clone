7f8ffc31c67b48367494659cff4fdda55d293766
kanji <kanjihtmt@gmail.com>
2015-09-23 13:29:57 +0900

    first commit
    以下を実装
    
    ・rails new
    ・Gemfileの設定
    ・initializer, localeの設定
    ・deviseでログイン画面作成
    ・基本的なレイアウトを調整

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module YauctionClone
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.controller_specs false
+      g.view_specs false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
