4d384f41a8af1fe4b50cefa6c46d7162d0b09af6
kanji <kanjihtmt@gmail.com>
2015-09-25 07:25:33 +0000

    通常の商品一覧、出荷商品一覧、出荷終了商品一覧が表示出来るように実装

--- app/controllers/mypage/products_controller.rb
+++ app/controllers/mypage/products_controller.rb
@@ -3,8 +3,7 @@ class Mypage::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    #@products = Product.where(status: Product::PUBLISHED).order(created_at: :desc)
-    @products = Product.order(created_at: :desc)
+    @products = Product.status(params[:status])
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,19 @@ class Product < ActiveRecord::Base
   PUBLISHED = 1.freeze
   UNUSED = 2.freeze
 
+  scope :status, ->(status) do
+    status = status.to_i unless status.blank?
+    case (status)
+      when PUBLISHED
+        where("status = ? and start_date <= ? and end_date >= ?", PUBLISHED, Time.now, Time.now)
+          .order(created_at: :desc)
+      when UNUSED
+        where("status = ? or end_date < ?", UNUSED, Time.now).order(created_at: :desc)
+      else
+        order(created_at: :desc)
+    end
+  end
+
   belongs_to :max_bidding, class_name: 'Bidding'
   belongs_to :seller, class_name: 'User'
   has_many :biddings, dependent: :destroy
