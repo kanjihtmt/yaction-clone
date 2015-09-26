bae2ba1faae4de83b84a9cdcd0e65266ff1f8a1b
kanji <kanjihtmt@gmail.com>
2015-09-26 17:38:30 +0900

    商品一覧のタイトルを適正なものに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def get_title_of_products(status)
+    case status.to_i
+      when Product::PUBLISHED
+        title = '出品中の商品一覧'
+      when Product::UNUSED
+        title = '出品終了分の商品一覧'
+      else
+        title = '商品一覧'
+    end
+
+    title
+  end
 end
