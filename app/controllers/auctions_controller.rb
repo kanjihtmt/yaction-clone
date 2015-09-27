f9a18a498b6f08f4979b39f9620fe7ddc288a08d
kanji <kanjihtmt@gmail.com>
2015-09-28 07:12:02 +0900

    オークション詳細のカルーセルを暫定的に実装

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,7 +1,9 @@
 class AuctionsController < ApplicationController
   def index
+    @products = Product.where(status: Product::PUBLISHED)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
