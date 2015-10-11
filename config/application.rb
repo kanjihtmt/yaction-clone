b2f11d42ad0a255140d46735da4aef08fb1250f4
kanji <kanjihtmt@gmail.com>
2015-10-12 07:50:51 +0900

    DBを検索時に正しい日時で検索しない(9時間ずれ)ので設定をUTCではなくシステム時刻に変更する

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module YauctionClone
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
