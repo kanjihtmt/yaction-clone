bb5ec893c8828e3d05aa472efd3f7b4c1cce1c69
kanji <kanjihtmt@gmail.com>
2015-10-14 06:35:19 +0000

    production環境にBasic認証を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'],
+    password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 
   protected
     def configure_permitted_parameters
