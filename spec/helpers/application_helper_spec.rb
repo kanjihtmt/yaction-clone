e12f401d9a86e2ee320193c4ddd31fa0094da56e
kanji <kanjihtmt@gmail.com>
2015-10-16 07:09:00 +0900

    ヘルパーのテストの雛形を作成

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe ApplicationHelper do
+  describe '#get_title_of_products' do
+  end
+
+  describe '#interval' do
+  end
+
+  describe '#expiration?' do
+  end
+
+  describe '#judged?' do
+  end
+end
