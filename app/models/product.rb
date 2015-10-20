61c3d6ffc7e1679e97b79a6e72a97bfc9bf1a64a
regonn <github@regonn.tokyo>
2015-10-20 22:09:19 +0900

    unless blank?

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,8 @@ class Product < ActiveRecord::Base
 
   scope :status, ->(status) do
     status = status.to_i unless status.blank?
+    # unless status.blank? は 空でないなら という否定の否定なので直感的に読みにくくなってしまうので
+    # if status.present? にすると 存在するなら という形になって読みやすくなります。
     case (status)
       when PUBLISHED
         where("status = ? and start_date <= ? and end_date >= ?", PUBLISHED, Time.now, Time.now)
