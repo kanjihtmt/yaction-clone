cb55889aab74fcd112712083a0aeb0246294962e
regonn <github@regonn.tokyo>
2015-10-26 13:49:16 +0900

    Rating リファクタ

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -13,6 +13,10 @@ class Rating < ActiveRecord::Base
   def self.aggregate(seller_id)
     aggregates = {}
     ['GOOD', 'NORMAL', 'BAD'].each do |status|
+      # 少しDRYに
+      # %w(week month year).each do |date_range|
+      #   aggregates["#{status.downcase}_count_of_#{date_range}".to_sym] = Rating.where(seller_id: seller_id,
+      #     value: "Rating::#{status}".constantize, created_at: Time.current.send("at_beginning_of_#{date_range}")..Time.now).count
       aggregates["#{status.downcase}_count_of_week".to_sym] = Rating.where(seller_id: seller_id,
         value: "Rating::#{status}".constantize, created_at: Time.now.at_beginning_of_week..Time.now).count
       aggregates["#{status.downcase}_count_of_month".to_sym] = Rating.where(seller_id: seller_id,
@@ -21,6 +25,7 @@ class Rating < ActiveRecord::Base
         value: "Rating::#{status}".constantize, created_at: Time.now.at_beginning_of_year..Time.now).count
       aggregates["#{status.downcase}_count_in_hundred".to_sym] =
         Rating.where(seller_id: seller_id).order(created_at: :desc).limit(100).map do |rate|
+          # そのRaitingの数(size)が知りたいのなら map でなくて select かも。
           rate.value == "Rating::#{status}".constantize
         end.size
       aggregates["#{status.downcase}_count_in_thousand".to_sym] =
