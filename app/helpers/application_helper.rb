67075eb9f11f7b7b95d40ad85f70921ca6aed62c
kanji <kanjihtmt@gmail.com>
2015-10-26 09:39:20 +0000

    指摘: case をスッキリを適用
    - 値が返されるか不安で記述している最後のtitleは不要
    - titleという変数自体もいらない

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,13 @@
 module ApplicationHelper
   def get_title_of_products(status)
     case status.to_i
-      when Product::PUBLISHED
-        title = '出品中の商品一覧'
-      when Product::UNUSED
-        title = '出品終了分の商品一覧'
-      else
-        title = '商品一覧'
+    when Product::PUBLISHED
+      '出品中の商品一覧'
+    when Product::UNUSED
+      '出品終了分の商品一覧'
+    else
+      '商品一覧'
     end
-
-    title
-
-    # ちゃんと値が返されるか不安になるかもしれませんが最後のtitleはこの場合不要です。
-    # そうなるとtitleという変数自体いらなくなるので
-    # def get_title_of_products(status)
-    #   case status.to_i
-    #   when Product::PUBLISHED
-    #     '出品中の商品一覧'
-    #   when Product::UNUSED
-    #     '出品終了分の商品一覧'
-    #   else
-    #     '商品一覧'
-    #   end
-    # end
-    # と書けます
   end
 
   def interval(type, from, to)
